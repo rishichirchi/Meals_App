c6f3e08d80440f5da61653d405f52493e098e381
Rishi Chirchi <rishiraj.chirchi@gmail.com>
2023-10-10 02:06:41 +0530

    added Navigation bar

---
+++ lib/screens/meal_details.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/meal.dart';
+import 'package:transparent_image/transparent_image.dart';
+
+class MealDetailsScreen extends StatelessWidget {
+  final Meal meal;
+  const MealDetailsScreen({super.key, required this.meal});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(meal.title, style: const TextStyle(color: Colors.white)),
+      ),
+      body: SingleChildScrollView(
+        child: Column(children: [
+          FadeInImage(
+            placeholder: MemoryImage(kTransparentImage),
+            image: NetworkImage(meal.imageUrl),
+            height: 300,
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          const SizedBox(height: 14),
+          Text(
+            'Ingredients',
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+          ),
+          for (final ingredient in meal.ingredients)
+            Text(
+              ingredient,
+              style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            const SizedBox(height: 24),
+          Text(
+            'Steps',
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+          ),
+          for (final step in meal.steps)
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+              child: Text(
+                step,
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onBackground,
+                    ),
+              ),
+            ),
+        ]),
+      ),
+    );
+  }
+}
--- lib/widget/meal_item.dart
+++ lib/widget/meal_item.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/screens/meal_details.dart';
 import 'package:transparent_image/transparent_image.dart';
+import 'package:meals/widget/meal_item_trait.dart';
 
 class MealItem extends StatelessWidget {
   final Meal meal;
+
+  String get complexityText {
+    return meal.complexity.name[0].toUpperCase() +
+        meal.complexity.name.substring(1);
+  }
+
+  String get affordabilityText {
+    return meal.affordability.name[0].toUpperCase() +
+        meal.affordability.name.substring(1);
+  }
 
   const MealItem({super.key, required this.meal});
   @override
@@ -16,7 +28,13 @@ class MealItem extends StatelessWidget {
       clipBehavior: Clip.hardEdge,
       elevation: 2,
       child: InkWell(
-        onTap: () {},
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (ctx) => MealDetailsScreen(meal: meal),
+            ),
+          );
+        },
         child: Stack(
           children: [
             FadeInImage(
@@ -51,7 +69,22 @@ class MealItem extends StatelessWidget {
                       height: 12,
                     ),
                     Row(
-                      children: [],
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        MealItemTrait(
+                          icon: Icons.schedule,
+                          label: '${meal.duration} min',
+                        ),
+                        const SizedBox(
+                          width: 12,
+                        ),
+                        MealItemTrait(icon: Icons.work, label: complexityText),
+                        const SizedBox(
+                          width: 12,
+                        ),
+                        MealItemTrait(
+                            icon: Icons.attach_money, label: affordabilityText)
+                      ],
                     )
                   ],
                 ),
